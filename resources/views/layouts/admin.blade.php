8ab45e5b03d7de6ace9da3b99d378776c4adf12a
Emmanuel Azarcon <emmazarcon@gmail.com>
2026-01-05 22:08:58 +0800

    Create admin.blade.php

---
+++ resources/views/layouts/admin.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin | Poseidonian</title>
+
+    @vite(['resources/css/admin.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    {{-- top nav here --}}
+    @yield('content')
+
+</body>
+</html>
