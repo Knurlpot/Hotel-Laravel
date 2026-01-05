25718417d78f7110b2d580083c41098fe4fac39c
Emmanuel Azarcon <emmazarcon@gmail.com>
2026-01-05 22:08:27 +0800

    Create receptionist.blade.php

---
+++ resources/views/layouts/receptionist.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Reception | Poseidonian</title>
+
+    @vite(['resources/css/reception.css', 'resources/js/app.js'])
+</head>
+<body>
+
+    {{-- shared top nav --}}
+    @yield('content')
+
+</body>
+</html>
